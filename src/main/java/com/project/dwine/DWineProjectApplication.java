75a3e0572a9f3f57c51eb7e621f7a3e456e255a3
HyunTax <sht4873@nate.com>
2021-11-09 15:11:53 +0900

    create project

---
+++ src/main/java/com/project/dwine/DWineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.dwine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DWineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DWineProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dwine/DWineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dwine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DWineProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
