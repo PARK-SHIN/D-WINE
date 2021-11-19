8900196a361430f86287d038812df39e5e9ced30
ahdrn2016 <lyu9801@naver.com>
2021-11-19 12:39:28 +0900

    주문관리 - 리스트 불러오기, 주문상태 조회

---
+++ src/main/java/com/project/dwine/orderManage/model/service/OrderManageService.java
@@ -0,0 +1,12 @@
+package com.project.dwine.orderManage.model.service;
+
+import java.util.List;
+
+import com.project.dwine.orderManage.model.vo.Purchase;
+
+public interface OrderManageService {
+
+	List<Purchase> selectOrderList();
+	
+	List<Purchase> stateChangeList(String state);
+}
