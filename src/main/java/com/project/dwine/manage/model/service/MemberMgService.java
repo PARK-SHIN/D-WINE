71f96877a91bbcd83d0b270159608fd3823e70f9
choi-su-jung <tnwjd839200@gmail.com>
2021-11-23 17:24:47 +0900

    회원제명 리스트 구현

---
+++ src/main/java/com/project/dwine/manage/model/service/MemberMgService.java
@@ -0,0 +1,11 @@
+package com.project.dwine.manage.model.service;
+
+import java.util.List;
+
+import com.project.dwine.member.model.vo.Member;
+
+public interface MemberMgService {
+
+	List<Member> selectMemberMgList();
+
+}
