975f1be871a468ce8fb8a2262bd50a37a4cdb107
ahdrn2016 <lyu9801@naver.com>
2021-12-02 01:11:36 +0900

    매출조회 - 페이징(조건 검색 포함x)

--- src/main/java/com/project/dwine/salesInquiry/controller/SalesInquiryController.java
+++ src/main/java/com/project/dwine/salesInquiry/controller/SalesInquiryController.java
@@ -1,14 +1,21 @@
 package com.project.dwine.salesInquiry.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.project.dwine.paging.PageInfo;
+import com.project.dwine.product.model.vo.Product;
 import com.project.dwine.salesInquiry.model.service.SalesInquiryService;
 import com.project.dwine.salesInquiry.model.vo.Total;
 
@@ -27,21 +34,68 @@ public class SalesInquiryController {
 	public ModelAndView selectTodayStatus(ModelAndView mv) {
 		
 		Total total = salesInquiryService.selectTodayStatus();
+		List<Total> yearSales = salesInquiryService.yearSales();
 
 		mv.addObject("total", total);
+		mv.addObject("yearSales", yearSales);
 		mv.setViewName("salesInquiry/list");
 		
 		return mv;
 	}
 	
 	@GetMapping("daily")
-	public Model dailyList(Model model) {
+	public Model dailyList(Model model, @RequestParam(value="page", required=false) String page) {
 		
-		List<Total> dailyList = salesInquiryService.dailyList();
+		int dailyListCount = salesInquiryService.getDailyListCount();
 		
+		int resultPage = 1;
+		
+		// 하지만 페이지 전환 시 전달 받은 현재 페이지가 있을 경우 해당 값을 page로 적용
+		if(page != null) {
+			resultPage = Integer.parseInt(page);
+		}
+		PageInfo pi = new PageInfo(resultPage, dailyListCount, 10, 10);
+		
+		int startRow = (pi.getPage() - 1) * pi.getBoardLimit() + 1;
+        int endRow = startRow + pi.getBoardLimit() - 1;
+		
+		List<Total> dailyList = salesInquiryService.dailyList(startRow, endRow);
+		
+		model.addAttribute("pi", pi);
 		model.addAttribute("dailyList", dailyList);
 		
 		return model;
+	}
+	
+	@PostMapping("daily")
+	@ResponseBody
+	public Map<String, Object> dailyListNext(Model model, @RequestParam(value="page", required=false) String page) {
+		
+		int dailyListCount = salesInquiryService.getDailyListCount();
+		
+		int resultPage = 1;
+		
+		// 하지만 페이지 전환 시 전달 받은 현재 페이지가 있을 경우 해당 값을 page로 적용
+		if(page != null) {
+			resultPage = Integer.parseInt(page);
+		}
+		PageInfo pi = new PageInfo(resultPage, dailyListCount, 10, 10);
+		
+		int startRow = (pi.getPage() - 1) * pi.getBoardLimit() + 1;
+        int endRow = startRow + pi.getBoardLimit() - 1;
+		
+		List<Total> dailyList = salesInquiryService.dailyList(startRow, endRow);
+		
+		System.out.println(pi);
+		for(Total t : dailyList) {
+			System.out.println(t);
+		}
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("pi", pi);
+		map.put("dailyList", dailyList);
+		
+		return map;
 	}
 	
 	
